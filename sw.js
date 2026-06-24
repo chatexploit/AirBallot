b71c74aff86bd315b2410fbdcd1540ac49faa2d6
Alphy <chat.exploit@gmail.com>
2026-06-24 08:19:14 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'v1_election_cache';
+const ASSETS = [
+  'index.html',
+  'voting.html',
+  'results.html',
+  'admin.html',
+  'manifest.json',
+  'logo.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
